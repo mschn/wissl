cdcebe2319c959c9a666d79b2a3b8ca0444974d6
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2012-07-16 13:20:47 +0200

    before 48a6173, /playlist/id/song/pos used to return an empty content when pos is out of range. Now it returns a 404 error. This changes the way the client handles the end of a playlist. Client now stops player on 404 error.

--- WebContent/player.js
+++ WebContent/player.js
@@ -270,7 +270,11 @@ var player = {};
 					}
 				},
 				error : function (xhr) {
-					wsl.ajaxError("Failed to get previous song in playlist", xhr);
+					if (xhr.status === 404) {
+						player.stop();
+					} else {
+						wsl.ajaxError("Failed to get previous song in playlist", xhr);
+					}
 				}
 			});
 		}
@@ -299,7 +303,11 @@ var player = {};
 					}
 				},
 				error : function (xhr) {
-					wsl.ajaxError("Failed to get next song in playlist", xhr);
+					if (xhr.status === 404) {
+						player.stop();
+					} else {
+						wsl.ajaxError("Failed to get next song in playlist", xhr);
+					}
 				}
 			});
 		}
