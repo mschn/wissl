1a91cc03ad1de5399d09a89b4f4a5c81202c772f
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2012-07-23 17:11:30 +0200

    improve indexer related tests further

--- test/fr/msch/wissl/test/TestIndexer.java
+++ test/fr/msch/wissl/test/TestIndexer.java
@@ -25,6 +25,8 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 
+import fr.msch.wissl.server.RuntimeStats;
+
 /**
  * Functional test for the following rest server endpoints:
  * <ul>
@@ -76,6 +78,14 @@ public class TestIndexer extends TServer {
 		get.addRequestHeader("sessionId", user_sessionId);
 		client.executeMethod(get);
 		Assert.assertEquals(401, get.getStatusCode());
+
+		// /folders: should be empty
+		get = new GetMethod(URL + "folders");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		assertEquals(200, get.getStatusCode());
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(0, obj.getJSONArray("folders").length());
 
 		// /folders/listing as user: 401
 		get = new GetMethod(URL + "folders/listing");
@@ -147,12 +157,71 @@ public class TestIndexer extends TServer {
 		client.executeMethod(post);
 		Assert.assertEquals(401, post.getStatusCode());
 
+		// /folders/add : directory does not exist: 404
+		post = new PostMethod(URL + "folders/add");
+		post.addParameter("directory", "/does/not/exist");
+		post.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(post);
+		Assert.assertEquals(404, post.getStatusCode());
+
+		// /folders/add : not a directory: 400
+		post = new PostMethod(URL + "folders/add");
+		post.addParameter("directory",
+				new File("test/data/1.mp3").getAbsolutePath());
+		post.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(post);
+		Assert.assertEquals(400, post.getStatusCode());
+
+		// /folders/add "/test/data"
+		f = new File("test/data");
+		RuntimeStats rt = new RuntimeStats();
+		rt.songCount.set(15);
+		rt.albumCount.set(5);
+		rt.artistCount.set(2);
+		rt.playlistCount.set(0);
+		rt.userCount.set(2);
+		rt.playtime.set(15);
+		rt.downloaded.set(0);
+		this.addMusicFolder(f.getAbsolutePath(), rt);
+
+		// check /folders
+		get = new GetMethod(URL + "folders?directory=" + f.getAbsolutePath());
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		assertEquals(200, get.getStatusCode());
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(f.getAbsolutePath(), obj.getJSONArray("folders")
+				.getString(0));
+
 		// /folders/remove as user: 401
 		post = new PostMethod(URL + "folders/remove");
 		post.addParameter("directory[]", "/");
 		post.addRequestHeader("sessionId", user_sessionId);
 		client.executeMethod(post);
 		Assert.assertEquals(401, post.getStatusCode());
+
+		// /folders/remove "/test/data"
+		post = new PostMethod(URL + "folders/remove");
+		post.addParameter("directory[]", f.getAbsolutePath());
+		post.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(post);
+		assertEquals(204, post.getStatusCode());
+
+		rt.songCount.set(0);
+		rt.albumCount.set(0);
+		rt.artistCount.set(0);
+		rt.userCount.set(2);
+		rt.playtime.set(0);
+		rt.downloaded.set(0);
+		this.checkStats(rt);
+
+		// /folders: should be empty
+		get = new GetMethod(URL + "folders");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		assertEquals(200, get.getStatusCode());
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(0, obj.getJSONArray("folders").length());
 
 	}
 }
