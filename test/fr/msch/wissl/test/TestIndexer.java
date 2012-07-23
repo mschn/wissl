3cbdeb0052db6c84e8d2b2759adb58219da56863
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2012-07-23 15:52:47 +0200

    add tests for /folders/listing in TestIndexer

--- test/fr/msch/wissl/test/TestIndexer.java
+++ test/fr/msch/wissl/test/TestIndexer.java
@@ -15,17 +15,25 @@
  */
 package fr.msch.wissl.test;
 
+import java.io.File;
+
 import junit.framework.Assert;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
 
 /**
  * Functional test for the following rest server endpoints:
  * <ul>
  * <li>/indexer/status
+ * <li>/indexer/rescan
  * <li>/folders
+ * <li>/folders/listing
+ * <li>/folders/add
+ * <li>/folders/remove
  * </ul>
  * 
  * @author mathieu.schnoor@gmail.com
@@ -42,6 +50,27 @@ public class TestIndexer extends TServer {
 		client.executeMethod(get);
 		Assert.assertEquals(401, get.getStatusCode());
 
+		// /indexer/status as admin: 200
+		get = new GetMethod(URL + "indexer/status");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		Assert.assertEquals(200, get.getStatusCode());
+		JSONObject obj = new JSONObject(get.getResponseBodyAsString());
+		// won't try to check the actual content of this object,
+		// since I can't predict easily and accurately if 
+		// the Indexer will be in Running or Sleeping state at a given time.
+		assertTrue(obj.has("running"));
+		assertTrue(obj.has("percentDone"));
+		assertTrue(obj.has("secondsLeft"));
+		assertTrue(obj.has("songsDone"));
+		assertTrue(obj.has("songsTodo"));
+
+		// /indexer/rescan as user: 401
+		PostMethod post = new PostMethod(URL + "indexer/rescan");
+		post.addRequestHeader("sessionId", user_sessionId);
+		client.executeMethod(post);
+		Assert.assertEquals(401, post.getStatusCode());
+
 		// /folders as user: 401
 		get = new GetMethod(URL + "folders");
 		get.addRequestHeader("sessionId", user_sessionId);
@@ -54,8 +83,65 @@ public class TestIndexer extends TServer {
 		client.executeMethod(get);
 		Assert.assertEquals(401, get.getStatusCode());
 
+		// /folders/listing on some file that does not exist: 404
+		get = new GetMethod(URL + "folders/listing?directory=/does/not/exist");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		assertEquals(404, get.getStatusCode());
+
+		File exp_home = new File(System.getProperty("user.home"));
+
+		// /folders/listing with no arg: homedir
+		get = new GetMethod(URL + "folders/listing");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		assertEquals(200, get.getStatusCode());
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(File.separator, obj.getString("separator"));
+		File dir = new File(obj.getString("directory"));
+		assertEquals(exp_home.getAbsolutePath(), dir.getAbsolutePath());
+		assertEquals(exp_home.getParentFile().getAbsolutePath(), dir
+				.getParentFile().getAbsolutePath());
+		assertTrue(obj.getJSONArray("listing").length() > 0);
+
+		// /folders/listing with arg '$ROOT'
+		get = new GetMethod(URL + "folders/listing?directory=$ROOT");
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		Assert.assertEquals(200, get.getStatusCode());
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(File.separator, obj.getString("separator"));
+		File[] dirs = File.listRoots();
+		assertEquals("", obj.getString("directory"));
+		assertEquals("$ROOT", obj.getString("parent"));
+		JSONArray arr = obj.getJSONArray("listing");
+		assertEquals(dirs.length, arr.length());
+		for (int i = 0; i < dirs.length; i++) {
+			// assumes the order is the same... is this always true?
+			File dd = new File(arr.getString(i));
+			assertEquals(dirs[i].getAbsolutePath(), dd.getAbsolutePath());
+		}
+
+		// lists test resources folder
+		File f = new File("test/data2");
+		get = new GetMethod(URL + "folders/listing?directory="
+				+ f.getAbsolutePath());
+		get.addRequestHeader("sessionId", admin_sessionId);
+		client.executeMethod(get);
+		obj = new JSONObject(get.getResponseBodyAsString());
+		assertEquals(File.separator, obj.getString("separator"));
+		dir = new File(obj.getString("directory"));
+		assertEquals(f.getAbsolutePath(), dir.getAbsolutePath());
+		assertEquals(f.getParentFile().getAbsolutePath(), dir.getParentFile()
+				.getAbsolutePath());
+		dirs = dir.listFiles();
+		arr = obj.getJSONArray("listing");
+		assertEquals(2, arr.length());
+		assertEquals(new File("test/data2/sub1").getAbsolutePath(), arr.get(0));
+		assertEquals(new File("test/data2/sub2").getAbsolutePath(), arr.get(1));
+
 		// /folders/add as user: 401
-		PostMethod post = new PostMethod(URL + "folders/add");
+		post = new PostMethod(URL + "folders/add");
 		post.addParameter("directory", "/");
 		post.addRequestHeader("sessionId", user_sessionId);
 		client.executeMethod(post);
@@ -69,5 +155,4 @@ public class TestIndexer extends TServer {
 		Assert.assertEquals(401, post.getStatusCode());
 
 	}
-
 }
