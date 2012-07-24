651e01226eca4ac02f70e5156d627a54122e4560
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2012-07-24 15:40:28 +0200

    fixed: missing Forbidden (403) exception mapper

--- src/fr/msch/wissl/server/Application.java
+++ src/fr/msch/wissl/server/Application.java
@@ -19,6 +19,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import fr.msch.wissl.server.exception.BadRequestMapper;
+import fr.msch.wissl.server.exception.ForbiddenExceptionMapper;
 import fr.msch.wissl.server.exception.IllegalArgumentMapper;
 import fr.msch.wissl.server.exception.MethodNotAllowedMapper;
 import fr.msch.wissl.server.exception.NotAcceptableMapper;
@@ -45,6 +46,7 @@ public class Application extends javax.ws.rs.core.Application {
 		set.add(NotAcceptableMapper.class);
 		set.add(SecurityErrorMapper.class);
 		set.add(IllegalArgumentMapper.class);
+		set.add(ForbiddenExceptionMapper.class);
 		return set;
 	}
 
