11dde5ddfc33047a39257f96460721ee2f331aae
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2012-08-02 01:04:05 +0200

    typo

--- src/main/webapp/wissl.js
+++ src/main/webapp/wissl.js
@@ -504,7 +504,7 @@ var wsl = wsl || {};
 				content += '<p>Copyright (c) 2012 Mathieu Schnoor</p>';
 				content += '<p>Distributed under the terms of the <a target="_blank"';
 				content += 'href="http://www.gnu.org/licenses/gpl.html">GNU GPL v3</a></p>';
-				content += '<p>Website: <a target="_blank" href="http://mschn.fr/wissl">http://mschn.fr/wissl</a></p>';
+				content += '<p>Website: <a target="_blank" href="http://msch.fr/wissl">http://msch.fr/wissl</a></p>';
 				content += '</div>';
 
 				content += '<table>';
